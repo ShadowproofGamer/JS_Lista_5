e7d0926c0eb36ac27b3c05340fb97a5d962f5a86
ShadowproofPC <266886@student.pwr.edu.pl>
2023-04-11 17:17:06 +0200

    zad3

--- lista5_1.py
+++ lista5_1.py
@@ -1,4 +1,4 @@
-import re, copy
+import re, logging, sys
 
 # 2a
 def to_dict(line:str):
@@ -33,18 +33,51 @@ def get_ipv4s_from_log(log:dict):
 def get_user_from_log(log:dict):
     return log.get("user")
 
+# 2d
+def get_message_type(desc:str):
+    login_successful_pattern = r'check pass' #?
+    login_failed_pattern = r'authentication failure'
+    connection_closed = r'^Connection closed|^Received disconnect'
+    wrong_password = r'^Failed password'
+    wrong_username = r'^Invalid user'
+    breach_attempt = r'POSSIBLE BREAK-IN ATTEMPT!'
+    if(re.search(string=desc, pattern=breach_attempt)): return "breach attempt"
+    elif(re.search(string=desc, pattern=wrong_username)): return "wrong username"
+    elif(re.search(string=desc, pattern=wrong_password)): return "wrong password"
+    elif(re.search(string=desc, pattern=connection_closed)): return "connection closed"
+    elif(re.search(string=desc, pattern=login_failed_pattern)): return "login failed"
+    elif(re.search(string=desc, pattern=login_successful_pattern)): return "login successful"
+    else: return "other"
+
+
 
 try: 
     file_path = input()
     file = open(file_path, "r")
     data_lines = [tmp.strip() for tmp in file.readlines()]
+
+    # 3
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout, encoding='utf-8')
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, encoding='utf-8')
+    logging.basicConfig(level=logging.WARNING, stream=sys.stdout, encoding='utf-8')
+    logging.basicConfig(level=logging.ERROR, stream=sys.stderr, encoding='utf-8')
+    logging.basicConfig(level=logging.CRITICAL, stream=sys.stderr, encoding='utf-8')
+
     for line in data_lines:
         #print(line)
         newl = to_dict(line)
-        #print(newl)
-        #print(get_ipv4s_from_log(newl))
-        #print(get_user_from_log(newl))
-        #print("\n")
+        print(newl)
+        print(get_ipv4s_from_log(newl))
+        print(get_user_from_log(newl))
+        #
+        type = get_message_type(newl.get("description"))
+        
+        logging.debug(len(line))
+        if(type == "login successful" or type == "connection closed"): logging.info(type)
+        elif(type == "login failed"): logging.warning(type)
+        elif(type == "wrong username" or type == "wrong password"): logging.error(type)
+        elif(type == "breach attempt"): logging.critical(type)
+        print("\n")
         pass
 
 
